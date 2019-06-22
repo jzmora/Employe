b5a466e5ae273c27318a3fab08e22a6e5b52f225
jzmora <jzmora@gmail.com>
2019-06-22 14:03:00 +0200

    Creado el enrutamiento(falta crear cada uno de los componenetes-vistas que se cargara con el enrutamiento)

--- employee/src/app/app-routing.module.ts
+++ employee/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
-
+/*import { HomeComponent } from './ home/ home.component';
+import { CounterComponent } from './ counter/ counter.component';
+import { FetchDataComponent } from './ fetch-data/ fetch-data.component';
+import { EmployeeComponent } from './ employee/ employee.component';*/
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  /*{ path: 'employee', component: EmployeeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent },*/
+  { path: '**', redirectTo: 'employee' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
